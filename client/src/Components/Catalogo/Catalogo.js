fdd567a5bd34faaa462ca026552b5c78bb6c5380
AlanCasella <alan.casella@hotmail.com.ar>
2020-12-11 01:11:30 -0300

    primera conexión del store

--- client/src/Components/Catalogo/Catalogo.js
+++ client/src/Components/Catalogo/Catalogo.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import obj from "./ObjetoTemporal.js";
 import ProductCard from "../ProductCard/ProductCard.js"
 import "./Catalogo.css";
+import {connect, useSelector} from "react-redux"
+import {getProducts, getCategories} from "../../store/actions/index.js"
 
 /* Componente a medio terminar. El CSS de ProductCard es necesario para que se vea bien la Card a la hora
 de renderizar. el CSS de categorías es un poco inestable y es necesario modificarlo cuando se pasen props,
 pero por ahora no quiero renegar mucho más, ya que no tengo idea cuantas categorías van a haber.
 El código y la lógica funcionan bien, salvo por la falta de props en la parte de categorías.*/
 
-export default function Catalogo() {
+function Catalogo(props) {
+ // const [state, setstate] = useState({ products: "", categories: "" });
+  // const state = useSelector((store) => store);
+  const [state, setState] = React.useState();
+
+  useEffect(() => {
+    setState(props.getProducts());
+  }, []);
+  
+  function handleClick(e) {
+    // console.log(props.getProducts(), "la funcion que viene de props")
+    // console.log(state, "soy el estado")
+    console.log(props.state, "soy el state.products de las props")
+  }
   return (
     <div id="Catalogo-Container">
       <div id="Catalogo-Lista-Container">
@@ -16,7 +31,7 @@ export default function Catalogo() {
         <li className="Catalogo-Lista-Item" style={{marginTop:30}}>Category</li>
         <li className="Catalogo-Lista-Item">Category</li>
         <li className="Catalogo-Lista-Item">Category</li>
-        <button className="Catalogo-btn">Browse All</button>
+        <button onClick={handleClick}className="Catalogo-btn">Browse All</button>
       </div>
 
       <div id="Catalogo-ProductCard-Container">
@@ -30,3 +45,19 @@ export default function Catalogo() {
     </div>
   );
 }
+
+function mapStateToProps(state) {
+    return {
+      state: state
+      // categories: state.categories
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getProducts: () => dispatch(getProducts()),
+    getCategories: () => dispatch(getCategories()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Catalogo)
